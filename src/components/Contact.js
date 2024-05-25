6a66d48a30dc9d353a14ea51ff1d6266423b69b8
“parulsachan86” <parulsachan86@gmail.com>
2024-05-25 22:45:15 +0530

    email sent added successfully

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,64 @@
 import React, { useState } from "react";
-
+import emailjs from "emailjs-com";
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
   };
 
   const handleSubmit = (e) => {
-    console.log(name, email, message);
-
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    console.log("User Data: ", data);
+    emailjs
+      .send("service_pq16j6p", "template_insqvfr", data, "bPnMv6c7dcxbVRyJs")
+      .then(
+        (response) => {
+          console.log("SUCCESS", response.status, response.text);
+          alert("Message sent succesfully");
+
+          // Reset the form
+          setData({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (err) => {
+          console.log("FAILED", err),
+            alert("Failed to send message, Please try again.");
+        }
+      );
   };
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
+
+  // const encode = (data) => {
+  //   return Object.keys(data)
+  //     .map(
+  //       (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+  //     )
+  //     .join("&");
+  // };
+
+  // const handleSubmit = (e) => {
+  //   console.log(name, email, message);
+
+  //   e.preventDefault();
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact", name, email, message }),
+  //   })
+  //     .then(() => alert("Message sent!"))
+  //     .catch((error) => alert(error));
+  // };
 
   return (
     <section id="contact" className="relative">
@@ -71,7 +105,6 @@ export default function Contact() {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            {" "}
             Contact me
           </h2>
           <div className="relative mb-4">
@@ -81,9 +114,9 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="name "
+              name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleChange}
             ></input>
           </div>
           <div className="relative mb-4">
@@ -95,7 +128,7 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -109,7 +142,7 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <button
