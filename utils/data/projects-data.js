615c9f8fe094ba7b0a99531aef01d3a46e96fec7
Parul Sachan <parulsachan86@gmail.com>
2025-07-21 21:38:47 +0530

    Contact us not implemented

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -4,7 +4,7 @@ export const projectsData = [
     name: "Teaching Assistant Management System",
     description:
       "Developed a role-based Teaching Assistant (TA) Management System to streamline interactions between professors and students, leveraging React.js for an interactive front-end and AWS Lambda for a scalable, serverless back-end. Utilized AWS CodePipeline for continuous integration and delivery.",
-    tools: ["React.js, AWS Lambda, AWS CodePipeline"],
+    tools: ["React.js", "AWS Lambda", "AWS CodePipeline"],
     role: "Front End Developer",
     keyFeatures: [
       "Enabled professors to manage TAship requirements, review applications, and accept or reject candidates via an intuitive interface.",
@@ -35,50 +35,6 @@ export const projectsData = [
     code: "",
     demo: "",
   },
-//   {
-//     id: 3,
-//     name: "AI Powered Real Estate",
-//     description:
-//       "My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.",
-//     tools: [
-//       "React",
-//       "Bootstrap",
-//       "SCSS",
-//       "Stripe",
-//       "Express",
-//       "TypeScript",
-//       "MongoDB",
-//       "Azure Blob",
-//       "OpenAI API",
-//       "Replicate AI",
-//       "Cronjob",
-//       "JWT",
-//     ],
-//     code: "",
-//     role: "Full Stack Developer",
-//     demo: "",
-//   },
-//   {
-//     id: 4,
-//     name: "Newsroom Management",
-//     description:
-//       "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-//     tools: ["NextJS", "Material UI", "Redux", "Sun Editor", "Calendar"],
-//     code: "",
-//     demo: "",
-//     role: "Full Stack Developer",
-//   },
 ];
 
-// Do not remove any property.
-// Leave it blank instead as shown below
 
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-// },
